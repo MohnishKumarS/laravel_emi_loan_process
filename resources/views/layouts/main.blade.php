4f0ca58378e6234a75e9a64933d6b924b48e0f07
MohnishKumarS <mohnish101998@gmail.com>
2024-08-01 16:06:40 +0530

    logo and title updated

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>EMI loan process</title>
+    <title>EMI loan Management</title>
       <!-- CSRF Token -->
       <meta name="csrf-token" content="{{ csrf_token() }}">
       <link rel="shortcut icon" href="{{asset('assets/img/emi-logo.png')}}" type="image/x-icon">
